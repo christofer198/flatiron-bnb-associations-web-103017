eb77aff57b7c104f9dee4195e34810b732bf21a1
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-13 23:01:53 +0000

    automatically backed up by learn

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,6 +5,7 @@ class Listing < ActiveRecord::Base
   # belongs_to :user, through: :reservation
   has_many :reservations
   has_many :reviews
+  has_many :guests, :class_name => 'User'
 
   #binding.pry
 end
