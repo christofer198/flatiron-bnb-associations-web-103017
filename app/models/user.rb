5d464daf71267f2803ee3258dbbe471d87ed27b6
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-13 22:45:39 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_many :listings, :foreign_key => 'host_id'
   has_many :reviews, :foreign_key => 'guest_id'
   has_many :reservations, :foreign_key => 'guest_id'
-  has_many :trips, :foreign_key => 'reservation'
+  has_many :trips, :class_name => "Reservation"
 end
