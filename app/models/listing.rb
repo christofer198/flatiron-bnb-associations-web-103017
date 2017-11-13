9b5c92c27c0ae1b665c0fa5d6db6383f9cdb3f54
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-13 23:06:28 +0000

    automatically backed up by learn

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,7 +5,7 @@ class Listing < ActiveRecord::Base
   # belongs_to :user, through: :reservation
   has_many :reservations
   has_many :reviews
-  has_many :guests, :class_name => 'Reservation'
+  has_many :guests, :class_name => 'Reservation', :foreign_key => 'list_id'
 
   #binding.pry
 end
