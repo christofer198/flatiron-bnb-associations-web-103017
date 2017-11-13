496b02ccce63fd2346d8e68b3237e66949c08859
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-13 22:39:47 +0000

    automatically backed up by learn

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,7 @@ require 'pry'
 class Listing < ActiveRecord::Base
   belongs_to :neighborhood
   belongs_to :host, :class_name => "User"
-  belongs_to :user, through: :reservation
+  # belongs_to :user, through: :reservation
   has_many :reservations
   has_many :reviews
 
