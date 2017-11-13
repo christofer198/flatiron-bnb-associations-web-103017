39cfaa9f9fd51d41364afecd387c711808b6cc73
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-13 22:56:24 +0000

    automatically backed up by learn

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,10 +3,9 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   belongs_to :listing
 
-  def checkin
-    # self.checkin.strftime('%m/%d/%Y')
+  
+    self.checkin.strftime('%m/%d/%Y')
     binding.pry
-  end
 
   def checkout
 
