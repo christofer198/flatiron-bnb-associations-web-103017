986670d544347a17140d735897a3083e192e99c9
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-13 23:20:12 +0000

    automatically backed up by learn

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
-  belongs_to :listing, through: :guest
+  belongs_to :listing, through: :guests
   belongs_to :reservation
 
 end
